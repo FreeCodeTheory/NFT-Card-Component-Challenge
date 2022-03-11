571f5fbd70f2d7e248c513e8e49a19b3ec96e499
Maxime FEVRE <mfevre@macbook-pro-de-maxime.home>
2022-03-11 10:28:48 +0100

    NFT Car Component Challenge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
-  content: ["./index.html"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-blue-body': '#14253D',
+        'dark-blue-card': '#1B335A',
+      },
+      screens: {
+        'mobile': '375px',
+        // => @media (min-width: 375px) { ... }
+  
+        'desktop': '1440px',
+        // => @media (min-width: 1440px) { ... }
+      },
+      boxShadow: {
+        'shadow-1': '0px 8px 2px 6px #000000',
+      }
+    },
   },
   plugins: [],
 }
